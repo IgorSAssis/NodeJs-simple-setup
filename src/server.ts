204560718642fa068399c74649d743aa64c738f2
IgorSA <igorsemphoski@gmail.com>
2020-11-18 15:25:49 -0300

    Add Typescript and ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request: Request, response: Response) => {
+    return response.send({ message: "Hello world!!" })
+})
+
+app.listen(3333, () => console.log("Listening on port 3333!"))
